2c103277162f4a815f57a397eb63b31d108b7438
DanGrewal1 <57231951+DanGrewal1@users.noreply.github.com>
2022-03-09 09:49:59 -0800

    added swift package manager

---
+++ README.md
@@ -0,0 +1,3 @@
+# AGEVideoLayout
+
+A description of this package.
---
+++ Tests/AGEVideoLayoutTests/AGEVideoLayoutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AGEVideoLayout
+
+final class AGEVideoLayoutTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AGEVideoLayout().text, "Hello, World!")
+    }
+}
